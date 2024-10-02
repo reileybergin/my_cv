509c8d0c0bb287a83effaa22f9d83bd3e0350f57
Reiley Bergin <reiley.bergin@gmail.com>
2024-10-02 10:21:59 -0400

    changed data to Excel file and removed unused functions

--- functions.R
+++ functions.R
@@ -1,35 +1,15 @@
 library(pander)
 library(stringr)
 library(dplyr)
-library(googlesheets4)
 library(lubridate)
 library(kableExtra)
-
-gs4_deauth()
-
-gscholar_stats <- function(url) {
-  cites <- get_cites(url)
-  return(paste(
-    'Citations:', cites$citations, '•',
-    'h-index:',   cites$hindex, '•',
-    'i10-index:', cites$i10index
-  ))
-}
-
-get_cites <- function(url) {
-  html <- xml2::read_html(url)
-  node <- rvest::html_nodes(html, xpath='//*[@id="gsc_rsb_st"]')
-  cites_df <- rvest::html_table(node)[[1]]
-  cites <- data.frame(t(as.data.frame(cites_df)[,2]))
-  names(cites) <- c('citations', 'hindex', 'i10index')
-  return(cites)
-}
+library(readxl)  
 
 get_cv_sheet <- function(sheet) {
-    return(read_sheet(
-        ss = 'https://docs.google.com/spreadsheets/d/1xyzgW5h1rVkmtO1rduLsoNRF9vszwfFZPd72zrNmhmU/edit?usp=sharing',
-        sheet = sheet
-    ))
+  return(read_excel(
+    path = 'data/cv.xlsx',
+    sheet = sheet
+  ))
 }
 
 make_ordered_list <- function(x) {
